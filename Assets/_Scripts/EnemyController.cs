3a2de178671b24cd783fcb6a680d43845f5b4860
Dilara <dilaraismn@gmail.com>
2023-02-15 18:06:07 +0300

    Enemy Following Path

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    private Path _path;
+    private int currentPoint;
+    private bool reachedEnd;
+    
+    void Start()
+    {
+        _path = FindObjectOfType<Path>();
+    }
+
+    void Update()
+    {
+        if (reachedEnd) return;
+        
+        transform.position = Vector3.MoveTowards(transform.position, _path.pathPoints[currentPoint].position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, _path.pathPoints[currentPoint].position) < 0.1f)
+        {
+            currentPoint = currentPoint + 1;
+            if (currentPoint >= _path.pathPoints.Length)
+            {
+                reachedEnd = true;
+            }
+        }
+    }
+}
