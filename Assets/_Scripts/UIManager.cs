b3c370d7700f561ad4c398fa29083797a866b36e
Dilara <dilaraismn@gmail.com>
2023-02-17 11:56:26 +0300

    Pause Game Completed

--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -2,25 +2,51 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject notEnoughGoldWarningText, levelCompleteScreen, levelFailScreen, towerButtons;
+    public GameObject notEnoughGoldWarningText, levelCompleteScreen, levelFailScreen, towerButtons, pauseScreen;
     public static UIManager instance;
     public TextMeshProUGUI goldText;
+    public string levelSelectScene, mainMenuScene;
     private void Awake()
     {
         instance = this;
     }
 
-    void Start()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseUnpause();
+        }
     }
 
-    void Update()
+    public void PauseUnpause()
     {
-        
+        if (pauseScreen.activeSelf == false)
+        {
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void LevelSelect()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(levelSelectScene);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(mainMenuScene);
     }
 }
